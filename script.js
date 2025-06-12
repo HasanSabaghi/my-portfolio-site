29be7ada433b3c9b4e71561550b2795364e746ae
Hasan Sabaghi <eng.hasansabaghi@gmail.com>
2025-06-12 19:07:46 +0330

    certificate element

--- script.js
+++ script.js
@@ -51,7 +51,20 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 
- 
+  function openModal(img) {
+    const modal = document.getElementById("imgModal");
+    const modalImg = document.getElementById("modalImage");
+    const caption = document.getElementById("caption");
+
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+    caption.textContent = img.alt;
+  }
+
+  function closeModal() {
+    document.getElementById("imgModal").style.display = "none";
+  }
+  
   
 
 
